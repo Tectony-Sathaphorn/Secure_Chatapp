cca12a3a269ae0d925bf22bb6fdc50d9f96e9349
Tectony-Sathaphorn <sathaphorn@tectony.co.th>
2025-03-27 18:18:37 +0700

    flutterRTCredeploy

--- .dart_tool/flutter_build/5c407149d61e0d693fab2629211e2f94/install_code_assets.d
+++
@@ -1,1 +0,0 @@
- C:\\Users\\mahaw\\Desktop\\BOSS\\AppChat2703\\appchat_25_3_2025\\appchat\\.dart_tool\\flutter_build\\5c407149d61e0d693fab2629211e2f94\\native_assets.json: 
---
+++ android/app/src/main/kotlin/com/example/new_secure_chat/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.new_secure_chat
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
